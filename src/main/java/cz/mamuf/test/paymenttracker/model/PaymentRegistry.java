af56ddd49e729c963f6d3cf26df530bc6924c83b
Adam Hosek <adam.hosek@mamuf.cz>
2016-03-06 14:09:05 +0100

    Added javadoc

--- src/main/java/cz/mamuf/test/paymenttracker/model/PaymentRegistry.java
+++ src/main/java/cz/mamuf/test/paymenttracker/model/PaymentRegistry.java
@@ -16,19 +16,37 @@ public class PaymentRegistry {
 	private final List<PaymentRegistryListener> listeners = new ArrayList<>();
 	private final List<Payment> payments = new ArrayList<>();
 
+	/**
+	 * {@link PaymentRegistry} listener. Implementors should not throw any
+	 * exceptions.
+	 */
 	public static interface PaymentRegistryListener {
+		/**
+		 * Executed at the end of {@link PaymentRegistry#add(Payment)} method.
+		 */
 		void paymentAdded(Payment payment);
 	}
 
+	/**
+	 * Registers a {@link PaymentRegistryListener}.
+	 */
 	public void addListener(final PaymentRegistryListener listener) {
 		listeners.add(listener);
 	}
 
+	/**
+	 * Adds a {@link Payment}.
+	 */
 	public void add(final Payment payment) {
 		payments.add(payment);
 		listeners.forEach(l -> l.paymentAdded(payment));
 	}
 
+	/**
+	 * Calculates total for payments with given {@link Currency}.
+	 *
+	 * @return Sum total of selected payments.
+	 */
 	public int getTotalByCurrency(final Currency currency) {
 		return payments.stream()
 				.filter(p -> p.getCurrency().equals(currency))
@@ -36,9 +54,16 @@ public class PaymentRegistry {
 				.sum();
 	}
 
+	/**
+	 * Calculates totals for payments grouped by their currencies.
+	 *
+	 * @return Totals mapped by {@link Currency}.
+	 */
 	public Map<Currency, Integer> getTotalsByCurrencies() {
-		// map by Currency and merge the values by adding them up, effectively
-		// calculating the totals
+		/*
+		 * map values by Currency and merge the values by adding them up,
+		 * effectively calculating the totals
+		 */
 		return payments.stream().collect(toMap(
 				Payment::getCurrency,
 				Payment::getValue,
